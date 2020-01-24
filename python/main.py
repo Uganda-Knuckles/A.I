d9fa3e79837ed977e3e3133a510257bc83a866a1
Uganda-Knuckles <59398982+Uganda-Knuckles@users.noreply.github.com>
2020-01-24 20:08:13 -0300

    Create main.py

---
+++ python/main.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+from chatterbot.trainers import ListTrainer
+from chatterbot import ChatBot
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+bot = Chatbot('A.I')
+
+bot.set_trainer(ListTrainer)
+
+for _file is os.listdir(
+
+with sr.Microphone() as s:
+  r.adjust_for_ambient_noise(s)
+  
+  while True:
+    audio = r.listen(s)
+    
+    speech = r.recognize_google(audio, language='en')
+    
+    print('You spoke :', speech)
+    
